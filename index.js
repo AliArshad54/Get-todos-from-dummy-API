1d5f7766ac09a575c600a9526d5707d53fd074bd
AliArshad54 <103595793+AliArshad54@users.noreply.github.com>
2022-08-22 09:43:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+document.querySelector('button').addEventListener('click' , (e) =>{
+    getCompletedtodos();
+})
+function getCompletedtodos(){
+ let values=[];
+unchecked=document.querySelectorAll('input[type="checkbox"]:checked');
+unchecked.forEach(chk=> values.push(chk.value));
+    console.log(values)
+}
+document.querySelector('#UnCheckAll').addEventListener('click',(e) => {
+    let checkboxes= document.querySelectorAll('input[type=checkbox]')
+    checkboxes.forEach(
+        function(checkbox) 
+        {
+        checkbox.checked=false;
+        }
+    )
+})
+document.querySelector('#CheckAll').addEventListener('click' , (e) => {
+    let checkboxes = document.querySelectorAll('input[type=checkbox]')
+    checkboxes.forEach(
+        function(checkbox)
+        {
+        checkbox.checked=true;
+        }
+    )
+})
+
+window.addEventListener('DOMContentLoaded', event=>{
+
+    div_todos=document.querySelector('#todos')
+    fetch('http://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(todos => {
+
+    todos.forEach(todo=> {
+            div_todos.innerHTML +=
+
+            `<input type="checkbox"  value="${todo.id}" id="${todo.id}" ${ todo.completed ? "completed":"" } /> 
+            <label for="${todo.id}"> ${todo.title}  </label><br/>`
+        
+        });
+        getCompletedtodos();
+      })
+     .catch(err => console.error(err));
+    }
+    )    
